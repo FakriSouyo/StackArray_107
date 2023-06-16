b0739c445e2fcae22cb8d44e5ae5bff443012b8a
Fakhri abdillah <fakriggwp@gmail.com>
2023-06-16 11:24:53 +0700

    15

--- StackArray/StackArray.cpp
+++ StackArray/StackArray.cpp
@@ -39,57 +39,59 @@ public:
 		top--; // step 3 decrement
 	}
 
-	//method for check if data is empty
-	bool empty() {
-		return (top == -1);
-	}
+    // method to check if the stack is empty
+    bool empty() {
+        return (top == -1);
+    }
 
-	void display() {
-		if (empty()) {
-			cout << "\nStack is empty. " << endl;
-		}
-		else {
-			for (int tmp = top; tmp >= 0; tmp--) {
-				cout << stack_array[tmp] << endl;
-			}
-		}
-	}
+    void display() {
+        if (empty()) {
+            cout << "\nStack is empty. " << endl;
+        }
+        else {
+            for (int tmp = 0; tmp <= top; tmp++) {
+                cout << stack_array[tmp] << endl;
+            }
+        }
+    }
 };
 
-	int main() {
-		StackArray s;
-		char ch;
-		while (true) {
-			cout << endl;
-			cout << "\n*** Stack Menu ***\n";
-			cout << "1. Push\n";
-			cout << "2. Pop\n";
-			cout << "3. Display\n";
-			cout << "4. Exit\n";
-			cout << "\nEnter your choice: ";
-			cin >> ch;
-			switch (ch) {
-			case '1': {
-				cout << "\nEnter a element: ";
-				int element;
-				s.push(element);
-				break;
-			}
-			case '2':
-				if (s.empty()) {
-					cout << "\nStack is empty. " << endl;
-					break;
-				}
-				s.pop();
-				break;
-			case '3':
-				s.display();
-				break;
-			case '4':
-				return 0;
-			default:
-				cout << "\nInvalid choice. " << endl;
-				break;
-			}
-		}
-	}
+int main() {
+    StackArray s;
+    char ch;
+    while (true) {
+        cout << endl;
+        cout << "\n*** Stack Menu ***\n";
+        cout << "1. Push\n";
+        cout << "2. Pop\n";
+        cout << "3. Display\n";
+        cout << "4. Exit\n";
+        cout << "\nEnter your choice: ";
+        cin >> ch;
+
+        switch (ch) {
+        case '1': {
+            int element;
+            cout << "\nEnter an element: ";
+            cin >> element;
+            s.push(element);
+            break;
+        }
+        case '2':
+            if (s.empty()) {
+                cout << "\nStack is empty. " << endl;
+                break;
+            }
+            s.pop();
+            break;
+        case '3':
+            s.display();
+            break;
+        case '4':
+            return 0;
+        default:
+            cout << "\nInvalid choice. " << endl;
+            break;
+        }
+    }
+}
